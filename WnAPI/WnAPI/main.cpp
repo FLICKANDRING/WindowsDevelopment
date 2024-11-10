6bc35cb905e9577d74739b62018f973fed03ae43
coole <coole@DESKTOP-OLAVJBB>
2024-11-10 14:24:38 +0300

    Copy text DONE

--- WnAPI/WnAPI/main.cpp
+++ WnAPI/WnAPI/main.cpp
@@ -80,6 +80,20 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
 		{
+		case IDC_BUTTON_COPY:
+		{
+		//1) Создаем буфер, через который будет выполняться копирование:
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE]{};
+		//2) Получаем дескрипторы текстовых полей, с которыми будем работать:
+			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+			HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
+
+			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+			
+		}
+		break;
 		case IDOK: MessageBox(hwnd, "Была нажата кнопка ОК", "Info", MB_OK | MB_ICONINFORMATION); break;
 		case IDCANCEL: EndDialog(hwnd, 0); break;
 		}
