9a59718da6a0f238d0d4fe9367580e659c15d2f7
Almae_ap73@top-academy.ru <Almae_ap73@top-academy.ru>
2024-11-24 14:26:31 +0300

    HomeWork DONE

--- WnAPI/MainWindow/main.cpp
+++ WnAPI/MainWindow/main.cpp
@@ -1,8 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <Windows.h>
+#include <cstdio>
+#include "resource.h"
 
-CONST CHAR g_sc_WINDOW_CLASS[] = "My Main Window";
+CONST CHAR g_sz_WINDOW_CLASS[] = "My Main Window";
 
 INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
@@ -15,14 +19,25 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	wClass.cbClsExtra = 0;
 	wClass.cbWndExtra = 0;    // cb... - CountBytes;
 
-	wClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	wClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
-	wClass.hCursor = LoadCursor(NULL, IDI_APPLICATION);
+	wClass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE (IDI_ICON_COMPUTER));
+	wClass.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE (IDI_ICON_FOLDER));
+
+	//wClass.hIcon = 
+		//(HICON)LoadImage(hInstance, "Ico\\computer_screen_icon_231558.ico",IMAGE_ICON,LR_DEFAULTSIZE,LR_DEFAULTSIZE,LR_LOADFROMFILE);
+		
+	//wClass.hIconSm = 
+		//(HICON)LoadImage(hInstance, "Ico\\folder_icon_231576.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
+		
+	//wClass.hCursor = LoadCursor(hInstance, MAKEINTRESOURCE (IDC_CURSOR1)); // НАРИСОВАННЫЙ СОБСТВЕННОРУЧНО ТУТ ЖЕ В СТУДИИ
+
+
+	wClass.hCursor = (HCURSOR)LoadImage(hInstance, "Cursors\\Call of Duty Simon Ghost & Knife Animated--cursor--SweezyCursors.ani",
+		IMAGE_CURSOR, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE); // ЗАГРУЗКА КУРСОРА ИЗ ПАПКИ!
 	wClass.hbrBackground = (HBRUSH)COLOR_WINDOW;
 
 	wClass.hInstance = hInstance;
 	wClass.lpszMenuName = NULL;
-	wClass.lpszClassName = g_sc_WINDOW_CLASS;
+	wClass.lpszClassName = g_sz_WINDOW_CLASS;
 	wClass.lpfnWndProc = (WNDPROC)WndProc;
 
 	if (!RegisterClassEx(&wClass))
@@ -33,14 +48,20 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 
 
 	// 2) Создание окна:
+	INT screen_width = GetSystemMetrics(SM_CXSCREEN);
+	INT sreen_heigth = GetSystemMetrics(SM_CYSCREEN);
+	INT window_width = screen_width * 3 / 4;
+	INT window_height = sreen_heigth * .75;
+	INT window_start_x = screen_width / 8;
+	INT window_start_y = sreen_heigth / 8;
 	HWND hwnd = CreateWindowEx
 	(
 		NULL,							//Ex-Styles
-		g_sc_WINDOW_CLASS,				//Class name
-		g_sc_WINDOW_CLASS,				//Window title
+		g_sz_WINDOW_CLASS,				//Class name
+		g_sz_WINDOW_CLASS,				//Window title
 		WS_OVERLAPPEDWINDOW,			//Window style. Такой стиль всегда задается для главного окна.
-		CW_USEDEFAULT, CW_USEDEFAULT,	//Position - положение окна при запуске
-		CW_USEDEFAULT, CW_USEDEFAULT,	//Size     - размер создаваемого окна.
+		window_start_x, window_start_y,	//Position - положение окна при запуске
+		window_width, window_height,	//Size     - размер создаваемого окна.
 		NULL,
 		NULL, //-------------------->	//hMenu:   - Для главного окна это ResoureID главного меню
 										//для дочернего окна (элемента какого-то окна) 
@@ -81,6 +102,29 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 	case WM_CREATE:
 		break;
+	case WM_MOVE:
+
+	case WM_SIZE:
+	{
+		RECT window_rect;
+		GetWindowRect(hwnd, &window_rect);
+		INT window_width = window_rect.right - window_rect.left;
+		INT window_heigt = window_rect.bottom - window_rect.top;
+
+		CONST INT SIZE = 256;
+		CHAR sz_title[SIZE]{};
+
+		sprintf
+		(
+			sz_title, "%s. Position:%ix%i; Size: %ix%i",
+			g_sz_WINDOW_CLASS,
+			window_rect.left, window_rect.top,
+			window_width, window_heigt
+		);
+
+		SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_title);
+	}	
+	break;
 	case WM_COMMAND:
 		break;
 	case WM_DESTROY:
